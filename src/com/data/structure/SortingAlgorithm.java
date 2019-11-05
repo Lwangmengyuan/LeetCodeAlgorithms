d5f1214917f60c16a4affc09a32061c9c343b73d
Lwangmengyuan <camillawmy@gmail.com>
2019-11-05 18:31:18 +0800

    2019.11.05第二次提交

--- src/com/data/structure/SortingAlgorithm.java
+++ src/com/data/structure/SortingAlgorithm.java
@@ -58,14 +58,12 @@ public class SortingAlgorithm {
     public int[] insertSort(int[] array){
         for(int i = 1; i < array.length; i++){
             int temp = array[i];
-            for(int j = i-1; j > 0; j--){
-                if(array[j] > temp){
-                    array[j+1] = array[j];
-                }
-                else{
-                    array[j+1] = temp;
-                }
+            int j = i -1;
+            while(j >= 0 && array[j] > temp){
+                array[j+1] = array[j];
+                j--;
             }
+            array[j+1] = temp;
         }
         return array;
     }
